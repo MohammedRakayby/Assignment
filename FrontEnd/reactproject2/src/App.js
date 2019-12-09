fdb598788c1243cca732fae560587106bde6e498
Mohammed Rakayby <mohammedrakayby@gmail.com>
2019-12-10 01:06:49 +0200

    Added Frontend protoType

---
+++ FrontEnd/reactproject2/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import FullWidthTabs from "./components/TabComponent";
+function App() {
+    return (
+        <div>
+            <FullWidthTabs />
+        </div>
+    )
+};
+
+export default App;
