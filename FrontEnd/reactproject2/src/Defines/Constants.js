d8ff40bcc4818afabc323f0c953894799ae62ce4
Mohammed Rakayby <mohamed.rakayby@asset.com.eg>
2019-12-11 19:51:07 +0200

    updated constants and configuration

---
+++ FrontEnd/reactproject2/src/Defines/Constants.js
@@ -0,0 +1,8 @@
+
+export const HOST_URL = 'http://localhost:8080';
+export const CREATE = "/create";
+export const UPDATE = "/update";
+export const DELETE="/delete";
+export const GET_ALL = "/all";
+export const EMPLOYEE_CONTROLLER = "/employees";
+export const DEPARTMENT_CONTROLLER = "/departments";
